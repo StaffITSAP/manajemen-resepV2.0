babe2f29fbeca80160b036d5acfd783aa3bcade9
nyawijicode <web1.hitechcomputer@gmail.com>
2026-09-09 11:44:35 +0700

    sync button visible to all account

--- app/Filament/Resources/PurchaseRequisitionResource/Pages/ListPurchaseRequisitions.php
+++ app/Filament/Resources/PurchaseRequisitionResource/Pages/ListPurchaseRequisitions.php
@@ -7,7 +7,7 @@ use App\Services\PurchaseRequisitions\SmartSync\PurchaseRequisitionSmartSync;
 use Filament\Actions;
 use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ListRecords;
-use Illuminate\Support\Facades\Auth;
+
 
 class ListPurchaseRequisitions extends ListRecords
 {
@@ -26,7 +26,6 @@ class ListPurchaseRequisitions extends ListRecords
                     : 'Sinkron Data Permintaan Barang')
                 ->icon('heroicon-o-arrow-path')
                 ->color(fn(PurchaseRequisitionSmartSync $smartSync): string => $smartSync->isRunning() ? 'gray' : 'primary')
-                ->visible(fn() => Auth::user()?->hasRole('superadmin'))
                 ->disabled(fn(PurchaseRequisitionSmartSync $smartSync): bool => $smartSync->isRunning())
                 ->extraAttributes(fn(PurchaseRequisitionSmartSync $smartSync): array => $smartSync->isRunning()
                     ? ['class' => 'opacity-60 cursor-not-allowed [&_svg]:animate-spin']
